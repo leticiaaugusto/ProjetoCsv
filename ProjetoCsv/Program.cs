6f1851458a90d22de15a57e40b129586b492ecde
User <User@DESKTOP-21BM20S>
2025-05-23 15:41:04 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoCsv/Program.cs
@@ -0,0 +1,86 @@
+﻿using Sistemafotovoltaico;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.IO;
+using System.Linq;
+
+namespace ProjetoCsv
+{ 
+    class Program
+    {
+    static void Main(string[] args)
+    {
+
+           Console.WriteLine("Informe a potência do gerador utilizando ponto como separador decimal:\n");
+
+
+
+            // Converte para double usando CultureInfo.InvariantCulture para aceitar ponto como separador decimal.
+           double Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            var sistemas = LerCSV("TabelaValores.csv");
+
+          
+            // Caso o usuário digite um valor aproximado (não exato) ele ainda consegue visualizar as info. (margem de erro).
+            var sistemaSelecionado = sistemas.FirstOrDefault(s => Math.Abs(s.PotenciaGerador - Valor) < 0.02); 
+
+
+            // Verifica se o valor digitado existe no arquivo csv, se não existir retorna o Console.
+
+            if (sistemaSelecionado == null)
+            {
+                Console.WriteLine("Gerador não encontrado com essa potência.");
+                return;
+            }
+
+            Console.WriteLine("---Soluções inteligentes On Grid---\n");
+            Console.WriteLine($" Potência Máxima do Gerador: {sistemaSelecionado.PotenciaMaxima} kWp.");
+            Console.WriteLine($" Número de Módulos: {sistemaSelecionado.NumeroModulos}");
+            Console.WriteLine($" Tipo de Inversor(s):{sistemaSelecionado.Inversores}");
+            Console.WriteLine($" Potência dos Inversores: {sistemaSelecionado.PotenciaInversores} kW.");
+            Console.WriteLine($" String Box: {sistemaSelecionado.StringBox}");
+            Console.WriteLine($" Cabos: {sistemaSelecionado.CabosVermelhoPreto}");
+            Console.WriteLine($" Par de Conectores: {sistemaSelecionado.Conectores}");
+            Console.WriteLine($" Tensão Máxima de Operação: {sistemaSelecionado.TensaoMaxima:F2} V.");
+            Console.WriteLine($" Corrente Máxima de Operação: {sistemaSelecionado.CorrenteMaxima} A.");
+            Console.WriteLine($" Tensão de Circuito Aberto: {sistemaSelecionado.TensaoAbertoMaxima:F2} V.");
+            Console.WriteLine($" Corrente de Curto Circuito: {sistemaSelecionado.CorrenteCurtoMaxima} A.");
+
+            Console.WriteLine("\n Aperte qualquer tecla para sair da tabela com os valores!");
+            Console.ReadKey();
+        }
+
+        // Método de leitura do csv utilizando lista
+            static List<SistemaFotovoltaico> LerCSV(string caminho)
+        {
+            var linhas = File.ReadAllLines("C:\\ProjetoCsv\\ProjetoCsv\\ProjetoCsv\\TabelaValores.csv");
+            var lista = new List<SistemaFotovoltaico>();
+
+
+            foreach (var linha in linhas.Skip(1)) // pula o cabeçalho do csv
+            {
+                var colunas = linha.Split(',');
+
+                var sistema = new SistemaFotovoltaico
+                {
+                    PotenciaGerador = double.Parse(colunas[0], CultureInfo.InvariantCulture),
+                    NumeroModulos = int.Parse(colunas[1]),
+                    Inversores = colunas[2],
+                    PotenciaInversores = double.Parse(colunas[3], CultureInfo.InvariantCulture),
+                    StringBox = colunas[4],
+                    CabosVermelhoPreto = colunas[5],
+                    Conectores = int.Parse(colunas[6]),
+                    EstruturaFixacao = int.Parse(colunas[7])
+                };
+
+                lista.Add(sistema);
+            }
+
+            return lista;
+        }
+    }
+    }
+
---
+++ ProjetoCsv/SistemaFotovoltaico.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sistemafotovoltaico
+{
+    public class SistemaFotovoltaico
+    {
+        public double PotenciaGerador { get; set; }
+        public int NumeroModulos { get; set; }
+        public required string Inversores { get; set; }
+
+        public double PotenciaInversores { get; set; }
+        public required string StringBox { get; set; }
+        public required string CabosVermelhoPreto{ get; set; }
+
+        public int Conectores { get; set; }
+        public int EstruturaFixacao { get; set; }
+
+
+        // valores por unidade de módulo.
+        public double Potencia { get; set; } = 330;
+        public double Tensao { get; set; } = 37.7;
+        public double Corrente { get; set; } = 8.76;
+        public double TensaoAberto { get; set; } = 45.9;
+        public double CorrenteCurto { get; set; } = 9.27;
+
+        // valores máximos por quantidades de módulo (série).
+
+        public double PotenciaMaxima => Potencia* NumeroModulos;
+        public double TensaoMaxima => Tensao * NumeroModulos; 
+        public double CorrenteMaxima => Corrente;
+        public double TensaoAbertoMaxima => TensaoAberto * NumeroModulos;
+        public double CorrenteCurtoMaxima => CorrenteCurto;
+        // usar "=>" indica que são propriedades calculadas.
+    }
+}
+
